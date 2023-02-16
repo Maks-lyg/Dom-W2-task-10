8a892f29c641151f2209dde7831d6f753a152cb5
Maks <maks-lug333@yandex.ru>
2023-02-17 07:13:46 +0900

    its working

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine());
+int num = 0;
+num = (x / 100) * 100;
+num = (x - num) / 10;
+Console.WriteLine($"Второе число -> {num}");
